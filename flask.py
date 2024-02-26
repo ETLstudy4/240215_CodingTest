6fdbe7f9ec8ee2ab740821e8a87eba2125260da5
hirobro <104831627+hirobro@users.noreply.github.com>
2024-02-26 20:31:36 +0900

    Update flask.py

--- flask.py
+++ flask.py
@@ -8,7 +8,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
 
 @app.route("/greet", methods=["POST"])
 def greet():
